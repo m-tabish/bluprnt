18036b3b2cc9e8a1b5767ef7a8e09e4fc843bad9
Tabish <mohdtabishkhan001@gmail.com>
2025-07-04 10:30:06 +0530

    set to email and added rate limiter

--- client/src/components/ResponsiveDialog.jsx
+++ client/src/components/ResponsiveDialog.jsx
@@ -1,27 +1,14 @@
 /* eslint-disable react/prop-types */
-import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger
-} from "@/components/ui/drawer"
-import { cn } from "@/lib/utils"
 import axios from "axios"
 import { useState } from "react"
 import { useSelector } from "react-redux"
 import { Button } from "./ui/button"
 import { Input } from "./ui/input"
-import { Label } from "./ui/label"
 
 function ResponsiveDialog({ submit, setSubmit }) {
 
     const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('')
-    const [twitterHandle, setTwitterHandle] = useState('')
+
     const [user, setUser] = useState("");
     const [submitted, setSubmitted] = useState(submit);
 
@@ -55,15 +42,15 @@ function ResponsiveDialog({ submit, setSubmit }) {
 
     async function handleSubmit(e) {
         e.preventDefault(); // Prevent form from reloading the page
-        const inputUser = { email, username, twitterHandle };
 
-        if (inputUser.email && inputUser.username) {
-            await submitDetails(inputUser)
-            setUser({ ...inputUser });
+
+        console.log(email);
+
+        if (email && email.includes("@")) {
+            await submitDetails(email)
+            setUser({ email });
             setEmail("");
-            setTwitterHandle("");
-            setUsername("");
-            // console.log(`User Registered!\nName: ${inputUser.username}\nEmail: ${inputUser.email}\nTwitter: ${inputUser.twitterHandle || 'N/A'}`);
+
         } else {
             alert("Kindly check the entered details.");
         }
@@ -71,86 +58,81 @@ function ResponsiveDialog({ submit, setSubmit }) {
 
 
 
-    if (submitted) {
-        return (
-            <div className="text-lg text-center flex flex-col  font-marker  items-center justify-center">
-                Thank you for joining the waitlist🩵🩵🩵
+    return (<div className="flex lg:flex-row  flex-col lg:w-2/3 items-center justify-center">
+
+        <Input className="w-full  lg:h-16 lg:w-1/3 drop-shadow-lg shadow-xl font-jetMono text-black lg:rounded-r-none bg-primary border-2 border-white placeholder:text-white" placeholder="Place your email here!" onChange={(e) => setEmail(e.target.value)}>
+        </Input>
+
+        <Button className=" bg-white/80 hover:text-primary   hover:bg-white h-16 lg:m-0 mt-4 text-primary font-marker lg:rounded-l-none " variant={"default"} onClick={((e) => handleSubmit(e))}>{submitted ? "Welcome to the club" : "Join the Waitlist"}
+        </Button>
+    </div>)
+    // return (
+    //     <Drawer className=" border-t-4 border-red flex justify-center items-center">
+    //         <DrawerTrigger className=" w-52 min-h-full  lg:w-64 lg:h-20   text-primary font-marker font-bold border-transparent border-2 text-md bg-white hover:bg-primary hover:border-2 hover:border-blue-600 active:border-white active:border-2 hover:text-white transition-all duration-300">
+    //             Join the waitlist
+    //         </DrawerTrigger>
+
+    //         <DrawerContent className="mx-auto w-[90%] sm:w-[70%] md:w-[50%] lg:w-1/2 p-4 font-bold">
+    //             <DrawerHeader>
+    //                 <DrawerTitle className="font-bold text-center text-2xl font-marker">Kindly fill this form</DrawerTitle>
+    //                 <DrawerDescription className="font-bold text-center text-md font-jetMono text-red-400">
+    //                     Note: We might reach out for feedback on beta version.
+    //                 </DrawerDescription>
+
+    //                 <form className={cn("grid items-center gap-6 mt-4")} onSubmit={handleSubmit}>
+    //                     <div className="grid gap-3">
+    //                         <Label htmlFor="name" className="text-md font-bold">Name</Label>
+    //                         <Input
+    //                             type="text"
+    //                             className="border border-black w-full"
+    //                             id="username"
+    //                             value={username}
+    //                             placeholder="John Doe"
+    //                             onChange={e => setUsername(e.target.value)}
+    //                             required
+    //                         />
+    //                     </div>
+
+    //                     <div>
+    //                         <Label htmlFor="email" className="text-md font-bold">Email</Label>
+    //                         <Input
+    //                             type="email"
+    //                             className="border border-black w-full"
+    //                             id="email"
+    //                             value={email}
+    //                             placeholder="bluprnt@gmail.com"
+    //                             onChange={e => setEmail(e.target.value)}
+    //                             required
+    //                         />
+    //                     </div>
+
+    //                     <div>
+    //                         <Label htmlFor="twitter" className="text-md font-bold">Twitter Handle (Optional)</Label>
+    //                         <Input
+    //                             type="text"
+    //                             className="border border-black w-full"
+    //                             id="twitter"
+    //                             value={twitterHandle}
+    //                             placeholder="@damnthesebugs"
+    //                             onChange={e => setTwitterHandle(e.target.value)}
+    //                         />
+    //                     </div>
+
+    //                     <DrawerFooter className="flex flex-col gap-2">
+    //                         <Button type="submit" className="w-full border border-white ">Submit</Button>
+    //                         <DrawerClose asChild>
+    //                             <Button variant="outline" className="w-full text-black">Cancel</Button>
+    //                         </DrawerClose>
+    //                     </DrawerFooter>
+    //                 </form>
 
 
-            </div>
+    //             </DrawerHeader>
+    //         </DrawerContent>
 
 
-        )
-    }
-
-
-    return (
-        <Drawer className=" border-t-4 border-red flex justify-center items-center">
-            <DrawerTrigger className=" w-52 min-h-full  lg:w-64 lg:h-20   text-primary font-marker font-bold border-transparent border-2 text-md bg-white hover:bg-primary hover:border-2 hover:border-blue-600 active:border-white active:border-2 hover:text-white transition-all duration-300">
-                Join the waitlist
-            </DrawerTrigger>
-
-            <DrawerContent className="mx-auto w-[90%] sm:w-[70%] md:w-[50%] lg:w-1/2 p-4 font-bold">
-                <DrawerHeader>
-                    <DrawerTitle className="font-bold text-center text-2xl font-marker">Kindly fill this form</DrawerTitle>
-                    <DrawerDescription className="font-bold text-center text-md font-jetMono text-red-400">
-                        Note: We might reach out for feedback on beta version.
-                    </DrawerDescription>
-
-                    <form className={cn("grid items-center gap-6 mt-4")} onSubmit={handleSubmit}>
-                        <div className="grid gap-3">
-                            <Label htmlFor="name" className="text-md font-bold">Name</Label>
-                            <Input
-                                type="text"
-                                className="border border-black w-full"
-                                id="username"
-                                value={username}
-                                placeholder="John Doe"
-                                onChange={e => setUsername(e.target.value)}
-                                required
-                            />
-                        </div>
-
-                        <div>
-                            <Label htmlFor="email" className="text-md font-bold">Email</Label>
-                            <Input
-                                type="email"
-                                className="border border-black w-full"
-                                id="email"
-                                value={email}
-                                placeholder="bluprnt@gmail.com"
-                                onChange={e => setEmail(e.target.value)}
-                                required
-                            />
-                        </div>
-
-                        <div>
-                            <Label htmlFor="twitter" className="text-md font-bold">Twitter Handle (Optional)</Label>
-                            <Input
-                                type="text"
-                                className="border border-black w-full"
-                                id="twitter"
-                                value={twitterHandle}
-                                placeholder="@damnthesebugs"
-                                onChange={e => setTwitterHandle(e.target.value)}
-                            />
-                        </div>
-
-                        <DrawerFooter className="flex flex-col gap-2">
-                            <Button type="submit" className="w-full border border-white ">Submit</Button>
-                            <DrawerClose asChild>
-                                <Button variant="outline" className="w-full text-black">Cancel</Button>
-                            </DrawerClose>
-                        </DrawerFooter>
-                    </form>
-
-
-                </DrawerHeader>
-            </DrawerContent>
-
-
-        </Drawer>
-    )
+    //     </Drawer>
+    // )
 }
 
 
