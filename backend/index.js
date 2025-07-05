971a79cf75accc237902fccd6a94a8d5eb07712a
Tabish <mohdtabishkhan001@gmail.com>
2025-07-05 12:36:34 +0530

    removed rate-limiter

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,22 @@ const app = express();
 const { generateContent } = require("./gemini/index");
 const { Project, Waitlist } = require("./db/mongo");
 const e = require("cors");
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-
+// const morgan = require('morgan');
+// const rateLimit = require('express-rate-limit');
+ 
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
-// Logging middleware
-app.use(morgan('dev'));
+// // Logging middleware
+// app.use(morgan('dev'));
 
-// Rate Limiting middleware
-const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 1 minute window
-    max: 10, // limit each IP to 10 requests per minute
-    message: 'Too many requests, please try again later.',
-});
-app.use(limiter);
+// // Rate Limiting middleware
+// const limiter = rateLimit({
+//     windowMs: 1 * 60 * 1000, // 1 minute window
+//     max: 10, // limit each IP to 10 requests per minute
+//     message: 'Too many requests, please try again later.',
+// });
+// app.use(limiter);
 
 app.use(cors({
     origin: '*',
