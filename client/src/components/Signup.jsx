3fa9a5af6a6d9c76d845fdd0dae6b051cfa5e525
Tabish <mohdtabishkhan001@gmail.com>
2026-01-26 03:25:28 +0530

    fix: signup button

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -8,6 +8,7 @@ import { Mail } from "lucide-react";
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import "../App.css";
+import googleIcon from "../assets/googleIcon.png";
 import navlogo from "../assets/navlogo.png";
 import { useAuth } from "../firebase/authContext/index";
 export default function Signup({ className, ...props }) {
@@ -153,18 +154,14 @@ export default function Signup({ className, ...props }) {
 
                     <div className="grid gap-4 sm:grid-cols-1 ">
                         <Button
-                            variant="outline" className="w-full text-white bg-[#ea4236]"
+                            variant="outline" className="w-full text-white bg-white/30 hover:bg-white/40"
 
                             onClick={onGoogleSignIn}
                             disabled={isSigningUp}
                             type="button"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4 mr-2">
-                                <path
-                                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                                    fill="currentColor" />
-                            </svg>
-                            {isSigningUp ? "Creating account..." : "Continue with Google"}
+
+                            {isSigningUp ? "Creating account..." : <div className="flex items-center gap-2 text-white font-semibold "><img className="w-4 h-4 mr-2 bg-white rounded-full p-0.5" src={googleIcon} alt="Google" />Sign in with Google</div>}
                         </Button>
                     </div>
                 </div>
@@ -176,6 +173,7 @@ export default function Signup({ className, ...props }) {
 }
 
 const CheckYourEmail = ({ className, ...props }) => {
+    const { email, setEmailSent } = props;
     return (
         <div className={cn("flex flex-col gap-6 justify-center items-center text-blue-500", className)} {...props}>
             <div className="flex flex-col items-center gap-4 max-w-md text-center">
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -22,7 +22,6 @@ const router = createBrowserRouter(
             path: "/waitlist/*",
             element: <Waitlist />
         },
-
         {
             path: "/login",
             // element: <App />
